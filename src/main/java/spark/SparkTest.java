1d37628297c42e309b973a26fb296976c962c307
phil <phil.zhang@pkfare.com>
2019-07-30 21:43:55 +0800

    some update

--- src/main/java/spark/SparkTest.java
+++ src/main/java/spark/SparkTest.java
@@ -9,19 +9,27 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.SparkSession.Builder;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.StreamingContext;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author phil.zhang
@@ -29,26 +37,41 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
  */
 public class SparkTest {
 
+  private static Logger logger = LoggerFactory.getLogger(SparkTest.class);
+
   private static Broadcast<List<String>> listBroadcast;
 
   private static ThreadPoolExecutor pool = new ThreadPoolExecutor(10,10,1000L, TimeUnit.SECONDS,new LinkedBlockingQueue<>(10));
 
+  private static Configuration conf ;
+  static  {
+    conf = new Configuration();
+    conf.set("fs.defaultFS", "hdfs://192.168.1.161:8020");
+    conf.set("ipc.client.connect.timeout", "60000");
+    System.setProperty("HADOOP_USER_NAME", "hdfs");
+  }
+
   public static void main(String[] args) throws IOException, InterruptedException {
-    SparkSession spark = SparkSession.builder().appName("broadCastTest").master("local[2]")
-        .getOrCreate();
-    listBroadcast = getBroadcast(spark);
+    Builder builder = SparkSession.builder().appName("broadCastTest");
 
+    if (null == args || args.length == 0) {
+      builder.master("local[3]");
+    }
+    SparkSession spark = builder.getOrCreate();
+    getBroadcast(spark);
     refresh(spark);
-
+//    List<String> list = listBroadcast.value();
 
     JavaStreamingContext jssc = new JavaStreamingContext(
         new StreamingContext(spark.sparkContext(), Durations.seconds(5)));
 
     JavaDStream<String> dStream = jssc.socketTextStream ("192.168.1.162",9999);
-    dStream.flatMap(x-> Lists.newArrayList(x.split(" ")).iterator())
+    dStream.map(x -> x+":"+listBroadcast.getValue())
         .foreachRDD(x-> {
-          x.foreach(str-> {
-            System.out.println(str + ":" + listBroadcast.value().size());
+          x.foreachPartition(str-> {
+            while (str.hasNext()) {
+              System.out.println(str.next());
+            }
           });
         });
 
@@ -60,7 +83,11 @@ public class SparkTest {
   private static Broadcast<List<String>> getBroadcast(SparkSession spark) throws IOException {
     JavaSparkContext sparkContext = new JavaSparkContext(spark.sparkContext());
     List<String> list = Lists.newArrayList();
-    BufferedReader reader = new BufferedReader(new FileReader("c:/zf/custom/list.txt"));
+    FileSystem fileSystem = FileSystem.get(conf);
+    FSDataInputStream inputStream = fileSystem
+        .open(new Path("hdfs://192.168.1.161:8020/test/socket/1.txt"));
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
     String[] s = reader.readLine().split(" ");
     for (String str : s) {
       list.add(str);
@@ -76,7 +103,7 @@ public class SparkTest {
         while (true) {
 
           try {
-            Thread.sleep(60*1000);
+            Thread.sleep(10*1000);
           } catch (InterruptedException e) {
             e.printStackTrace();
           }
@@ -84,13 +111,18 @@ public class SparkTest {
           List<String> list = Lists.newArrayList();
           JavaSparkContext sparkContext = new JavaSparkContext(spark.sparkContext());
           try {
-            BufferedReader reader = new BufferedReader(new FileReader("c:/zf/custom/list.txt"));
+            FileSystem fileSystem = FileSystem.get(conf);
+            FSDataInputStream inputStream = fileSystem
+                .open(new Path("hdfs://192.168.1.161:8020/test/socket/1.txt"));
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//            BufferedReader reader = new BufferedReader(new FileReader("c:/zf/custom/list.txt"));
 
             String[] s = reader.readLine().split(" ");
             for (String str : s) {
               list.add(str);
             }
-            System.out.println("------------");
+            System.out.println("list size:" + list.size());
             listBroadcast.unpersist();
             listBroadcast= sparkContext.broadcast(list);
           } catch (IOException e) {
--- src/main/java/t/KafkaTest.java
+++ src/main/java/t/KafkaTest.java
@@ -1,8 +1,12 @@
 package t;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.pkfare.bigdata.common.spark.kafka.KafkaUtil;
+import com.pkfare.collect.common.serialization.ProtoSerializerUtil;
+import com.pkfare.collect.entity.EsLogEntity;
+import com.pkfare.collect.entity.TransData;
 import java.util.Iterator;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -16,14 +20,14 @@ import org.apache.kafka.common.serialization.StringSerializer;
  * @author phil.zhang
  * @date 2019/7/25
  */
-public class FanTest {
+public class KafkaTest {
 
 
 
   public static void main(String[] args) {
-    KafkaProducer<String, String> producer = KafkaUtil
+/*    KafkaProducer<String, String> producer = KafkaUtil
         .initProducer("192.168.1.147:9092,192.168.1.148:9092,192.168.1.149:9092",
-            StringSerializer.class, StringSerializer.class);
+            StringSerializer.class, StringSerializer.class);*/
 
 /*    Map<String, Object> params = Maps.newHashMap();
     params.put("bootstrap.servers", "192.168.1.147:9092,192.168.1.148:9092,192.168.1.149:9092");
@@ -40,7 +44,7 @@ public class FanTest {
 
     Map<String, Object> params1 = Maps.newHashMap();
     params1.put("bootstrap.servers", "192.168.1.147:9092,192.168.1.148:9092,192.168.1.149:9092");
-    params1.put("group.id", "test");
+    params1.put("group.id", "test1");
     // 如果已提交的offset时，从提交的offset开始消费；无提交的offset时，从头开始消费
     params1.put("auto.offset.reset", "earliest");
     params1.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
@@ -48,13 +52,14 @@ public class FanTest {
 
 
     KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(params1);
-    kafkaConsumer.subscribe(Lists.newArrayList("shopping-dwd"));
-    ConsumerRecords<String, String> records = kafkaConsumer.poll(1000);
-    Iterator<ConsumerRecord<String, String>> iterator = records.iterator();
-    while (iterator.hasNext()) {
-      System.out.println(iterator.next().value());
-    }
-    System.out.println(records.count());
+    kafkaConsumer.subscribe(Lists.newArrayList("air-api-pricing-records-dwd"));
+
+      ConsumerRecords<String, String> records = kafkaConsumer.poll(1000);
+      Iterator<ConsumerRecord<String, String>> iterator = records.iterator();
+      while (iterator.hasNext()) {
+        System.out.println(iterator.next());
+      }
+
   }
 
 
